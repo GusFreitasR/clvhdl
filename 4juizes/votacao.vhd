255dd472b5493d2aa741178667248bb2f50b1d2b
Gustavo de Freitas Rodrigues <gustavo_freitas_rodrigues@outlook.com>
2024-12-19 02:30:24 -0300

    Atualização dos 4 juizes

--- 4juizes/votacao.vhd
+++ 4juizes/votacao.vhd
@@ -2,7 +2,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
 entity votacao is
     Port (
         j0, j1, j2, j3 : in STD_LOGIC; 
@@ -11,22 +10,20 @@ entity votacao is
 end votacao;
 
 architecture Behavioral of votacao is
-    signal total_votos : integer range 0 to 4; 
-	 signal votos_concat : STD_LOGIC_VECTOR(3 downto 0); 
 begin
     process (j0, j1, j2, j3)
     begin
-		  votos_concat <= j0 & j1 & j2 & j3;
-        total_votos <= to_integer(unsigned(votos_concat));
-        if total_votos > 2 then
-            vd <= '1';
-            vm <= '0';
-        elsif total_votos = 2 then
-            vd <= '1';
-            vm <= '1';
-        else
-            vd <= '0';
-            vm <= '1';
-        end if;
+		
+		  if((j1='1' and j3='1' and j0='1') or (j0='1' and j1='1' and j2='1') or (j0='1' and j3='1' and j2='1') or (j1='1' and j2='1' and j3='1')) then
+			  vd <= '1';
+			  vm <= '0';
+		  
+		  elsif(not((j1='1' or j3='1' or j0='1') and (j0='1' or j1='1' or j2='1') and (j0='1' or j3='1' or j2='1') and (j1='1' or j2='1' or j3='1'))) then
+			  vd <= '0';
+			  vm <= '1';
+		  else
+			  vd <= '1';
+			  vm <= '1';
+		  end if;
     end process;
 end Behavioral;
